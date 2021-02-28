5b3d580f52bad745a71ce07ee32b1f98311f1fcd
Marisa Tjoe <marisa.tjoe@rea-group.com>
2021-02-28 11:31:02 +1100

    Remove layouts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
 import express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res) {
-  res.render("index", { title: "Express" });
+router.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 module.exports = router;
